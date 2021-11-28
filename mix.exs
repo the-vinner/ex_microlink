a99ea38d6a2fff9c6883cb809169e1183b42fec3
Vince Roy <vincer0117@gmail.com>
2021-11-28 10:49:08 -0500

    0.2.0

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExMicrolink.MixProject do
   def project do
     [
       app: :ex_microlink,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
